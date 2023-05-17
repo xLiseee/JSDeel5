013c24c98328c3fc017b5a82aa3d9235398b01fc
liseversmissenpxl <lise.versmissen@student.pxl.be>
2023-05-17 09:16:44 +0200

    Revert "Initial commit"
    This reverts commit ff807aa15a4c5c886dee8d84e682c4718f950fe5.

--- oefening1.js
+++
@@ -1,6 +0,0 @@
-document.getElementsByTagName("button")[0].addEventListener('click', tekstWeergeven);
-
-function tekstWeergeven() {
-    let tekst = document.getElementsByTagName("input")[0].value;
-    document.getElementsByTagName("p")[0].innerHTML = tekst;
-}
--- oefening2.js
+++
@@ -1,8 +0,0 @@
-document.getElementsByTagName("button")[0].addEventListener('click', tekstWeergeven);
-
-function tekstWeergeven() {
-    let nieuweP = document.createElement("p");
-    let nieuwePTekst = document.createTextNode(document.getElementsByTagName("input")[0].value);
-    nieuweP.appendChild(nieuwePTekst);
-    document.getElementsByTagName("body")[0].appendChild(nieuweP);
-}
